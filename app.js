32cb5c43103dabfcd6c61bbeb789f496978a7005
Naresh <mnaresh4813@gmail.com>
2024-11-08 19:48:43 +0530

    check routes for production

--- app.js
+++ app.js
@@ -67,6 +67,9 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTES
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
